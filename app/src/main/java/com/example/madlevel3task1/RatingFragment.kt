3361be437c15caf4df9f9722b689e7ade73496b6
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-09-28 13:27:29 +0200

    Everything up to the texting of sending the Bundle to the summary fragment

--- app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -22,5 +24,24 @@ class RatingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bt_summary.setOnClickListener {
+            navigateToSummary();
+        }
+
+        showRandomAssessableGame();
+    }
+
+    private fun navigateToSummary() {
+        val args = Bundle();
+        args.putFloat(ARG_GAME_RATING, rb_gameScore.rating);
+        args.putString(ARG_GAME_NAME, tv_gameTitle.text.toString());
+
+        findNavController().navigate(R.id.action_ratingFragment_to_summaryFragment);
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("one", "two", "three").random();
+
+        tv_gameTitle.text = randomGame;
     }
 }
